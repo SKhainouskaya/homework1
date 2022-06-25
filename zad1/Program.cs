9338c4df540475c8e88f6d1ba29415aa16ebc704
svetlana <s.khainouskaya@gmail.com>
2022-06-25 18:40:36 +0300

    1task

---
+++ zad1/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 5;
+int b = 7;
+Console.WriteLine ("первое число " + a);
+Console.WriteLine ("второе число " + b);
+if (a>b)
+{
+    Console.WriteLine ("первое число больше второго ");
+    Console.WriteLine ("второе число меньше первого ");
+}
+else
+{
+    Console.WriteLine ("второе число больше первого ");
+    Console.WriteLine ("первое число меньше второго ");
+}
