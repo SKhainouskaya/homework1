92bfa938d2f2eaea9f93d97d75c8fc0c9cf91b1a
svetlana <s.khainouskaya@gmail.com>
2022-06-25 18:49:18 +0300

    3task

---
+++ zad3/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 59;
+Console.WriteLine ("число " + a);
+if (a % 2 == 1)
+{
+    Console.WriteLine ("число является нечетным");
+}
+else 
+{
+    Console.WriteLine ("число является четным");
+}
