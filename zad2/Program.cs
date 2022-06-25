e71f70adf5ac3d52db3320b0aaa2ef060721d18b
svetlana <s.khainouskaya@gmail.com>
2022-06-25 18:45:20 +0300

    2task

---
+++ zad2/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 2;
+int b = 3;
+int c = 7;
+Console.WriteLine ("первое число " + a);
+Console.WriteLine ("второе число " + b);
+Console.WriteLine ("третье число " + c);
+if (a>b & a>c)
+{
+    Console.WriteLine ("max - первое число");
+}
+else if (b>a & b>c)
+{
+    Console.WriteLine ("max - второе число");
+}
+else if (c>a & c>b)
+{
+    Console.WriteLine ("max - третье число");
+}
+
